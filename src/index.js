66ab8fb6163e55892e40c8bfba4675bddeb044e9
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-10-27 02:09:00 -0300

    index js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './reset.css';
+import './styles/reset.css';
 
 import App from './App';
+import Header from './components/header';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
